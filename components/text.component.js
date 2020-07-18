d3b1fa818a944f492c698e92124955f4f8897e3d
rakibtg <rakibtg@gmail.com>
2020-07-19 00:59:26 +0600

    text component wrapper

--- components/text.component.js
+++ components/text.component.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Colors from '../constants/colors';
 
-export default TextComponent = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Text</Text>
-    </View>
-  );
+const TextComponent = props => {
+  const {
+    color = 'black',
+    size = 'md',
+    bold = false,
+    lineHeight = null,
+    children
+  } = props;
+  
+  const textStyle = [];
+
+  styles[color] && textStyle.push(styles[color]);
+  styles[size] && textStyle.push(styles[size]);
+  bold && textStyle.push(styles.bold);
+  lineHeight && textStyle.push({ lineHeight });
+
+  return <Text style={textStyle}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  black: {
+    color: Colors.black
   },
+  white: {
+    color: Colors.white
+  },
+  blue: {
+    color: Colors.blue
+  },
+  sm: {
+    fontSize: 12,
+  },
+  md: {
+    fontSize: 14
+  },
+  lg: {
+    fontSize: 18
+  },
+  bold: {
+    fontWeight: 'bold'
+  }
 });
+
+export default TextComponent;
