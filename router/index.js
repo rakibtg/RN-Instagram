25b684c82b357b7829828795ffb3d1dad85c936b
rakibtg <rakibtg@gmail.com>
2020-07-19 17:06:53 +0600

    header menu

--- router/index.js
+++ router/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { navigationRef } from '../nagivation';
+import Button from '../components/button.component';
 
-import StatusScreen from '../screens/status.screen';
+import PostScreen from '../screens/post.screen';
 import ProfileScreen from '../screens/profile.screen';
 
 const Stack = createStackNavigator()
@@ -12,8 +14,16 @@ export default function Router () {
   return (
     <NavigationContainer ref={navigationRef}>
       <Stack.Navigator>
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="Status" component={StatusScreen} />
+        <Stack.Screen 
+          name="Profile" 
+          component={ProfileScreen} 
+          options={{
+            headerRight: () => <View style={{marginHorizontal: 20}}>
+              <Button padding={10} iconName="ios-menu" />
+            </View>
+          }}
+        />
+        <Stack.Screen name="Post" component={PostScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
