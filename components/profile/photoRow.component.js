659c615e4d4cdde44f057245d2bfa41ffce68601
rakibtg <rakibtg@gmail.com>
2020-07-19 17:09:26 +0600

    dispatch navigate on press with proper payload

--- components/profile/photoRow.component.js
+++ components/profile/photoRow.component.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { View, Dimensions, StyleSheet } from 'react-native';
+import { View, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import Photo from '../photo.component';
+import { navigate } from '../../store/actions/app.action';
 
 const IMAGE_WIDTH = Dimensions.get("window").width / 3;
 
 const PhotoRowComponent = ({ data }) => {
   const { item } = data;
+  const dispatch = useDispatch();
+
+  const handlePhotoPress = data => {
+    dispatch(navigate('Post', data));
+  }
+
   return <View style={styles.row}>
     {
-      item.map((photo, index) => <Photo 
-        key={index}
-        uri={photo.photo.small} 
-        width={IMAGE_WIDTH} 
-        height={IMAGE_WIDTH}
-      />)
+      item.map((photo, index) => <TouchableOpacity key={index} onPress={() => handlePhotoPress(photo)}>
+        <Photo 
+          uri={photo.photo.small} 
+          width={IMAGE_WIDTH} 
+          height={IMAGE_WIDTH}
+        />
+      </TouchableOpacity>)
     }
   </View>
 }
