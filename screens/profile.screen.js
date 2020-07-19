64e573e8cbe8dc5c56c53c75f4b0e4a072e21423
rakibtg <rakibtg@gmail.com>
2020-07-19 12:12:35 +0600

    refactored profile component

--- screens/profile.screen.js
+++ screens/profile.screen.js
@@ -1,60 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, SectionList, Dimensions } from 'react-native';
+import { SectionList } from 'react-native';
 import Layout from '../components/layout.component';
-// import Gallery from '../components/gallery.component';
+import PhotoRow from '../components/profile/photoRow.component';
+import Section from '../components/profile/section.component';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { setUser } from '../store/actions/app.action';
 import { setPosts } from '../store/actions/post.action';
-import ProfileCard from '../components/profileCard.component';
-
-import Photo from '../components/photo.component';
-const IMAGE_WIDTH = Dimensions.get("window").width / 3;
-import { profileDataChunk } from '../helpers';
-
-const translateToProfileSections = (posts = []) => {
-  const sectionData = [
-    { title: 'PROFILE_CARD', data: [] },
-    { title: 'PHOTO_NAVIGATION', data: profileDataChunk(posts, 3) }
-  ];
-  return sectionData
-}
-
-const Item = ({data}) => {
-  const { item } = data;
-  return <View style={{flex: 1, flexDirection: 'row'}}>
-    {
-      item.map((photo, index) => <Photo 
-        key={index}
-        uri={photo.photo.small} 
-        width={IMAGE_WIDTH} 
-        height={IMAGE_WIDTH}
-      />)
-    }
-  </View>
-};
-
-const Section = ({data}) => {
-  if (data.title === 'PHOTOS') return null;
-  else if (data.title === 'PROFILE_CARD') return <ProfileCard />
-  else if (data.title === 'PHOTO_NAVIGATION') return <View style={{flex: 1, padding: 10, backgroundColor: '#fff'}}>
-    <Text>Ok.</Text>
-  </View>
-}
-
-const Gallery = () => {
-  const { posts } = useSelector(state => state.post, shallowEqual);
-  const sections = translateToProfileSections(posts)
-  return <SectionList 
-    bounces={false}
-    sections={sections}
-    removeClippedSubviews={true}
-    stickySectionHeadersEnabled={true}
-    keyExtractor={(item, index) => index.toString()}
-    renderItem={item => <Item data={item} />}
-    renderSectionHeader={({ section }) => <Section data={section} />}
-    showsVerticalScrollIndicator={false}
-  />
-}
+import { translateToProfileSections } from '../helpers';
 
 const ProfileScreen = ({ navigation }) => {
   const dispatch = useDispatch();
@@ -64,33 +16,21 @@ const ProfileScreen = ({ navigation }) => {
     dispatch(setPosts());
   }, [dispatch]);
 
-  return <Gallery />
+  const { posts } = useSelector(state => state.post, shallowEqual);
+  const sections = translateToProfileSections(posts);
 
-  /*return (
-    <Layout navigation={navigation} title="rakibtg">
-      <ProfileCard />
-      <Gallery />
-    </Layout>
-  );*/
+  return <Layout navigation={navigation} title="rakibtg" renderView>
+    <SectionList 
+      bounces={false}
+      sections={sections}
+      removeClippedSubviews={true}
+      stickySectionHeadersEnabled={true}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={item => <PhotoRow data={item} />}
+      renderSectionHeader={({ section }) => <Section data={section} />}
+      showsVerticalScrollIndicator={false}
+    />
+  </Layout>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16
-  },
-  item: {
-    backgroundColor: "#f9c2ff",
-    padding: 20,
-    marginVertical: 8
-  },
-  header: {
-    fontSize: 32,
-    backgroundColor: "#fff"
-  },
-  title: {
-    fontSize: 24
-  }
-});
 
 export default ProfileScreen;
